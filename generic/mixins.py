08a84d304ee23f6f56275da63b08a3fd5e00c451
Serg <ochkarik1983@mail.ru@>
2019-07-13 22:09:01 +0300

    G

--- generic/mixins.py
+++ generic/mixins.py
@@ -2,15 +2,21 @@ from django.views.generic.base import ContextMixin
 from django.conf import settings
 from category.models import Category
 from taggit.models import Tag
+from news.models import New
+from movies.models import Movie
 
 
 class CategoryListMixin(ContextMixin):
 	tags=Tag.objects.all()
+	last_news=New.objects.all()[0:3]
+	last_movie=Movie.objects.all()[0:3]
 
 	def get_context_data(self,**kwargs):
 		context=super(CategoryListMixin,self).get_context_data(**kwargs)
 		context["current_url"]=self.request.path
 		context["tags"]=self.tags
+		context["last_news"]=self.last_news
+		context["last_movie"]=self.last_movie
 		context["categories"]=Category.objects.all()
 		return context
 

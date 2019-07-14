2c9b0b2799be1f2d2bcde3ef8c0cf98551a85419
Serg <ochkarik1983@mail.ru@>
2019-07-14 23:01:46 +0300

    G

--- gintare/forms.py
+++ gintare/forms.py
@@ -3,13 +3,13 @@ from django import forms
 
 class FeedbackForm(forms.ModelForm):
     name = forms.CharField( label="",widget=forms.TextInput(
-            attrs={'class': 'input-md round form-control ', 'placeholder': ' Ваше имя'}
+            attrs={'class': 'form-control dvt-shadow-2 dvt-shadow-focus-4 dvt-transition m-b-2', 'placeholder': ' Ваше имя'}
         ))
     email = forms.EmailField( label="",widget=forms.EmailInput(
-            attrs={'class': 'input-md round form-control', 'placeholder': 'Ваша почта'}
+            attrs={'class': 'form-control dvt-shadow-2 dvt-shadow-focus-4 dvt-transition m-b-2', 'placeholder': 'Ваша почта'}
         ))
     message = forms.CharField( label="",widget=forms.Textarea(
-            attrs={'class': 'input-md round form-control', 'placeholder': 'Ваше сообщение','rows':'0','cols':'0','style':'height: 84px;'}
+            attrs={'class': 'form-control dvt-shadow-2 dvt-shadow-focus-4 dvt-transition m-b-2', 'placeholder': 'Ваше сообщение','rows':'0','cols':'0','style':'height: 84px;'}
         ))
     class Meta:
         model = Feedback

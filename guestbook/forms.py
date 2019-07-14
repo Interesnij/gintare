4d1e45296e5eed26fc3fdb3374ed6d47e4a575bc
Serg <ochkarik1983@mail.ru@>
2019-07-14 21:32:17 +0300

    G

--- guestbook/forms.py
+++ guestbook/forms.py
@@ -2,6 +2,11 @@
 from guestbook.models import Guestbook
 
 class GuestbookForm(forms.ModelForm):
+	content = forms.CharField(
+        label="",widget=forms.Textarea(
+                attrs={'class': 'form-control', 'placeholder': 'Напишите что-нибудь','rows':'8','cols':'0','style':'height: 84px;'}
+            )
+    )
 	class Meta:
 		model=Guestbook
-		exclude=['user', ] 
+		exclude=['user', ]

74b29e03f16e841fce2ac841addef06774d78794
Serg <ochkarik1983@mail.ru@>
2019-07-14 23:05:06 +0300

    G

--- gintare/forms.py
+++ gintare/forms.py
@@ -9,7 +9,7 @@ class FeedbackForm(forms.ModelForm):
             attrs={'class': 'form-control dvt-shadow-2 dvt-shadow-focus-4 dvt-transition m-b-2', 'placeholder': 'Ваша почта'}
         ))
     message = forms.CharField( label="",widget=forms.Textarea(
-            attrs={'class': 'form-control dvt-shadow-2 dvt-shadow-focus-4 dvt-transition m-b-2', 'placeholder': 'Ваше сообщение','rows':'0','cols':'0','style':'height: 84px;'}
+            attrs={'class': 'form-control dvt-shadow-2 dvt-shadow-focus-4 dvt-transition m-b-2', 'rows':'8', 'placeholder': 'Ваше сообщение','rows':'0','cols':'0','style':'height: 84px;'}
         ))
     class Meta:
         model = Feedback
